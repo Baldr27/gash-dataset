c62add21bfeada4d79dbeeca2a580a139a26c169
gjara08 <g.jara08@ufromail.cl>
2025-08-27 22:53:28 -0400

    I don't know what is happening anymore

---
+++ docs/js/duckdb/arrow.js
@@ -0,0 +1,3 @@
+// Load Apache Arrow from CDN and re-export it
+import * as arrow from 'https://cdn.jsdelivr.net/npm/apache-arrow@14.0.2/+esm';
+export * from 'https://cdn.jsdelivr.net/npm/apache-arrow@14.0.2/+esm';
